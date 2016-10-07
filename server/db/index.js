90718edfb51dc511ed6fe6d10461096f47add19b
willsimmons <willsimmons@gmail.com>
2016-10-07 14:58:22 -0700

    add connection to mysql

--- server/db/index.js
+++ server/db/index.js
@@ -3,5 +3,13 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  // password: null? may not need this
+  database: 'chat'
+});
+//export this?
+connection.connect();
 
 
